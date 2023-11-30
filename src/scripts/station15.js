309505b7c361858a1d41d1768844da1abb8772d9
KEN-4 <cuuw1561@icloud.com>
2023-11-30 19:39:17 +0900

    Clear station15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -2,15 +2,27 @@ async function getData() {
   // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
+  const result = await test();
+  return result
+}
+
+function test() {
   const userList = [
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
-  return await result
+
+  return new Promise(resolve => {
+    setTimeout(() => {
+      const fullNames = userList.map(buildFullName);
+      resolve(fullNames);
+    }, 3000); // 3秒後に解決
+  });
 }
 
-function test() {
-  return
+function buildFullName(data) {
+  return {
+      ...data,
+      full_name: `${data.family_name} ${data.first_name}`
+  };
 }
-
